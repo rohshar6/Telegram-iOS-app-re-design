174753b2436cf0099234ce00f5c95e1f92bc15df
rohshar6 <rohshar6@cisco.com>
2022-04-02 21:55:40 +0530

    inital commit

---
+++ iOS chat application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS chat application
+//
+//  Created by rohshar6 on 02/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
